055a09cc37631f8c71c24a782ef5b792b513f868
Marcelo Duarte Trevisani <marceloduartetrevisani@gmail.com>
2020-01-07 17:37:07 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,18 @@
 from setuptools import find_packages, setup
 
-
-def main():
-    setup(
-        name="grayskull",
-        packages=find_packages(where="src"),
-        package_dir={"": "src"},
-        use_scm_version={"write_to": "src/grayskull/_version.py"},
-        setup_requires=["setuptools-scm>=3.4", "setuptools>=42.0"],
-        install_requires=["requests", "pyyaml"],
-        extras_require={"testing": ["pytest"]},
-        url="https://github.com/marcelotrevisani/grayskull",
-        license="MIT",
-        author="Marcelo Duarte Trevisani",
-        author_email="marceloduartetrevisani@gmail.com",
-        description="Skeletor's main goal is to conquer the mysterious fortress of"
-        " Castle Grayskull. If he succeeds, Skeletor would be able to conquer not"
-        " only Eternia, but the whole universe.",
-    )
-
-
-if __name__ == "__main__":
-    main()
+setup(
+    name="grayskull",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    use_scm_version={"write_to": "src/grayskull/_version.py"},
+    setup_requires=["setuptools-scm>=3.4", "setuptools>=42.0"],
+    install_requires=["requests", "pyyaml"],
+    extras_require={"testing": ["pytest"]},
+    url="https://github.com/marcelotrevisani/grayskull",
+    license="MIT",
+    author="Marcelo Duarte Trevisani",
+    author_email="marceloduartetrevisani@gmail.com",
+    description="Skeletor's main goal is to conquer the mysterious fortress of"
+    " Castle Grayskull. If he succeeds, Skeletor would be able to conquer not"
+    " only Eternia, but the whole universe.",
+)
