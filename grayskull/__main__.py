dfccd290e4b7aad8c31593627da122c1a4753c67
Marcelo Duarte Trevisani <marceloduartetrevisani@gmail.com>
2020-02-14 08:04:43 +0000

    Possibility to call grayskull to generate multiple recipes

--- grayskull/__main__.py
+++ grayskull/__main__.py
@@ -6,9 +6,10 @@ from grayskull.base.factory import GrayskullFactory
 def main():
     parser = argparse.ArgumentParser(description="Grayskull - Conda recipe generator")
     parser.add_argument(
-        "repo_type",
-        nargs=2,
-        help="Specify the repository (pypi, cran) and the package name.",
+        "repo_type", nargs=1, help="Specify the repository (pypi, cran).",
+    )
+    parser.add_argument(
+        "packages", nargs="+", help="Specify the packages name.",
     )
     parser.add_argument(
         "--heman", "--shera", default=False, action="store_true", dest="grayskull_power"
@@ -29,10 +30,11 @@ def main():
     if args.grayskull_power:
         print("By the power of Grayskull...\nI have the power!")
 
-    recipe = GrayskullFactory.create_recipe(
-        args.repo_type[0], args.repo_type[1], args.version
-    )
-    recipe.generate_recipe(args.output)
+    for pkg_name in args.packages:
+        recipe = GrayskullFactory.create_recipe(
+            args.repo_type[0], pkg_name, args.version
+        )
+        recipe.generate_recipe(args.output)
 
 
 if __name__ == "__main__":
