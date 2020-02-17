622630a025d26b2764d48afb6183fe5a61add6d1
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2020-02-17 09:00:08 +0000

    Improvements on package discovery for setup.py (#41)
    * Improvements on package discovery for setup.py
    
    * Exclude test file from flake8
    
    * Add stdlib-list to azure

---
+++ tests/data/foo_imports.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+import numpy as np
+
+
+def foo():
+    import pandas
+
+
+def bar():
+    from requests import get
---
+++ tests/test_utils.py
@@ -0,0 +1,27 @@
+import os
+
+from grayskull.utils import (
+    get_all_modules_imported_script,
+    get_std_modules,
+    get_vendored_dependencies,
+)
+
+
+def test_get_std_modules():
+    std_modules = get_std_modules()
+    assert "sys" in std_modules
+    assert "os" in std_modules
+    assert "ast" in std_modules
+    assert "typing" in std_modules
+
+
+def test_get_all_modules_imported_script(data_dir):
+    all_imports = get_all_modules_imported_script(
+        os.path.join(data_dir, "foo_imports.py")
+    )
+    assert sorted(all_imports) == sorted(["numpy", "pandas", "requests", "os", "sys"])
+
+
+def test_get_vendored_dependencies(data_dir):
+    all_deps = get_vendored_dependencies(os.path.join(data_dir, "foo_imports.py"))
+    assert sorted(all_deps) == sorted(["numpy", "pandas", "requests"])
