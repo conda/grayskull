730e8ba87f3d6c5b0ff8ee844ef64b4aea024bc4
Marcelo Duarte Trevisani <marceloduartetrevisani@gmail.com>
2020-02-20 10:55:22 +0000

    Add more tests

--- grayskull/base/extra.py
+++ grayskull/base/extra.py
@@ -3,7 +3,7 @@ from subprocess import check_output
 import requests
 
 
-def _get_git_current_user_metadata() -> dict:
+def get_git_current_user_metadata() -> dict:
     git_out = check_output(["git", "config", "user.name"])
     return requests.get(
         url="https://api.github.com/search/users", params={"q": git_out.strip()},
@@ -12,10 +12,9 @@ def _get_git_current_user_metadata() -> dict:
 
 def get_git_current_user() -> str:
     try:
-        github_search = _get_git_current_user_metadata()
+        github_search = get_git_current_user_metadata()
         if github_search["total_count"] == 1:
-            github_login = github_search["items"][0]["login"]
-            return github_login
-    except Exception:
+            return github_search["items"][0]["login"]
+    except Exception as err:  # noqa
         pass
     return "AddYourGitHubIdHere"
