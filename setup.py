3487e2884d2d7f317cff83b2300b08055d21356f
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2020-02-27 12:42:39 +0000

    Make black happy

--- setup.py
+++ setup.py
@@ -3,11 +3,7 @@ from setuptools import setup
 setup(
     name="grayskull",
     packages=["grayskull", "grayskull.base", "grayskull.pypi"],
-    entry_points={
-        "console_scripts": [
-            "grayskull = grayskull.__main__:main",
-        ]
-    },
+    entry_points={"console_scripts": ["grayskull = grayskull.__main__:main"]},
     use_scm_version={"write_to": "grayskull/_version.py"},
     setup_requires=["setuptools-scm", "setuptools>=30.3.0"],
     python_requires=">=3.7",
