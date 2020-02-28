b0c8437567b1dd664c35791291e3a145de386101
Marcelo Duarte Trevisani <marceloduartetrevisani@gmail.com>
2020-02-28 09:13:40 +0000

    Fix Grayskull version __version__ Add option to get the grayskull version from command line improvements on pypi cmds options for command line

--- grayskull/__main__.py
+++ grayskull/__main__.py
@@ -1,12 +1,15 @@
 import argparse
 import logging
 import os
+import sys
 
 import colorama
 from colorama import Fore, Style
 from colorama.ansi import clear_screen
 
+import grayskull
 from grayskull.base.factory import GrayskullFactory
+from grayskull.cli.parser import parse_pkg_name_version
 
 colorama.init(autoreset=True)
 logging.basicConfig(format="%(levelname)s:%(message)s")
@@ -14,11 +17,18 @@ logging.basicConfig(format="%(levelname)s:%(message)s")
 
 def main():
     parser = argparse.ArgumentParser(description="Grayskull - Conda recipe generator")
-    parser.add_argument(
-        "repo_type", nargs=1, help="Specify the repository (pypi, cran).",
+    sub_parser = parser.add_subparsers(help="Options to generate PyPI recipes")
+    pypi_cmds = sub_parser.add_parser("pypi", help="Generate recipes based on PyPI")
+    pypi_cmds.add_argument(
+        "pypi_packages", nargs="+", help="Specify the PyPI packages name.", default=""
     )
     parser.add_argument(
-        "packages", nargs="+", help="Specify the packages name.",
+        "--version",
+        "-v",
+        default=False,
+        action="store_true",
+        dest="version",
+        help="Grayskull version",
     )
     parser.add_argument(
         "--heman", "--shera", default=False, action="store_true", dest="grayskull_power"
@@ -30,11 +40,12 @@ def main():
         default=".",
         help="Path to where the recipe will be created",
     )
-    parser.add_argument(
-        "--version", "-v", default="", dest="version", help="Package version "
-    )
-
     args = parser.parse_args()
+
+    if args.version:
+        print(grayskull.__version__)
+        sys.exit()
+
     logging.debug(f"All arguments received: args: {args}")
     print(Style.RESET_ALL)
     print(clear_screen())
@@ -44,16 +55,15 @@ def main():
             f"{Style.BRIGHT}I have the power!"
         )
 
-    for pkg_name in args.packages:
+    for pkg_name in args.pypi_packages:
         logging.debug(f"Starting grayskull for pkg: {pkg_name}")
         print(
             f"{Fore.GREEN}\n\n"
             f"#### Initializing recipe for "
-            f"{Fore.BLUE}{pkg_name} ({args.repo_type[0]}) {Fore.GREEN}####\n"
+            f"{Fore.BLUE}{pkg_name} (pypi) {Fore.GREEN}####\n"
         )
-        recipe = GrayskullFactory.create_recipe(
-            args.repo_type[0], pkg_name, args.version
-        )
+        pkg_name, pkg_version = parse_pkg_name_version(pkg_name)
+        recipe = GrayskullFactory.create_recipe("pypi", pkg_name, pkg_version)
         recipe.generate_recipe(args.output)
         print(
             f"\n{Fore.GREEN}#### Recipe generated on "
---
+++ tests/cli/test_parser.py
@@ -0,0 +1,11 @@
+from grayskull.cli.parser import parse_pkg_name_version
+
+
+def test_parse_pkg_name_version():
+    pkg_name, pkg_version = parse_pkg_name_version("pytest=5.3.5")
+    assert pkg_name == "pytest"
+    assert pkg_version == "5.3.5"
+
+    pkg_name, pkg_version = parse_pkg_name_version("pytest")
+    assert pkg_name == "pytest"
+    assert pkg_version is None
