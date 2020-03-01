ca726bc3666d03b646b83b370bc91491149d6522
Marcelo Duarte Trevisani <marceloduartetrevisani@gmail.com>
2020-03-01 02:07:41 +0000

    Fix parse pkg name and version when there is two =

--- grayskull/cli/parser.py
+++ grayskull/cli/parser.py
@@ -3,5 +3,5 @@ from typing import Optional, Tuple
 
 
 def parse_pkg_name_version(pkg_name: str) -> Tuple[str, Optional[str]]:
-    pkg = re.match(r"([a-zA-Z0-9\.-_]+)=+([a-zA-Z0-9\.-_]+)", pkg_name)
+    pkg = re.match(r"([a-zA-Z0-9\-_\.]+)=+([a-zA-Z0-9\-_\.]+)", pkg_name)
     return pkg.groups() if pkg else (pkg_name, None)
--- tests/cli/test_parser.py
+++ tests/cli/test_parser.py
@@ -6,6 +6,10 @@ def test_parse_pkg_name_version():
     assert pkg_name == "pytest"
     assert pkg_version == "5.3.5"
 
+    pkg_name, pkg_version = parse_pkg_name_version("pytest==5.3.5")
+    assert pkg_name == "pytest"
+    assert pkg_version == "5.3.5"
+
     pkg_name, pkg_version = parse_pkg_name_version("pytest")
     assert pkg_name == "pytest"
     assert pkg_version is None
