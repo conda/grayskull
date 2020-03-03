72e8a2759eaf8623c406f6a25a6a845e0e5165ce
Marcelo Duarte Trevisani <marceloduartetrevisani@gmail.com>
2020-03-03 06:02:25 +0000

    Fix lgtm complains

--- grayskull/__init__.py
+++ grayskull/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ["__version__"]
 
 try:
-    from ._version import version as __version__
+    from ._version import version as __version__  # lgtm [py/import-own-module]
 except ImportError:
     __version__ = "unknown"
