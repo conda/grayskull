c1bbeb5a383422fe8982dfedbd28cda2609e9260
Mahe Iram Khan <65779580+ForgottenProgramme@users.noreply.github.com>
2021-10-20 20:43:20 +0530

    Update grayskull/pypi/pypi.py
    Co-authored-by: Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>

--- grayskull/pypi/pypi.py
+++ grayskull/pypi/pypi.py
@@ -143,7 +143,7 @@ class PyPi(AbstractRecipeModel):
         return archive_tarball_url
 
     @staticmethod
-    def _download_sdist_pkg(sdist_url: str, dest: str, name: str = None):
+    def _download_sdist_pkg(sdist_url: str, dest: str, name: Optional[str] = None):
         """Download the sdist package
 
         :param sdist_url: sdist url
