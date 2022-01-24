8d811c9001526ebc533bbde877116851e9702a10
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2022-01-24 22:24:44 +0000

    Fix recipe maintainers (#286)

--- tests/cli/test_cli_recipes.py
+++ tests/cli/test_cli_recipes.py
@@ -1,7 +1,10 @@
+from souschef.recipe import Recipe
+
 from grayskull.__main__ import main
 
 
-def test_loop_deps_nipy(tmpdir):
+def test_loop_deps_nipy_and_maintainers(tmpdir, mocker):
+    mocker.patch("grayskull.__main__.get_git_current_user", return_value="GIT_USER")
     out_folder = tmpdir.mkdir("out")
     main(["pypi", "nipy=0.4.2", "-o", str(out_folder), "--download"])
     nipy_folder = out_folder / "nipy"
@@ -11,3 +14,6 @@ def test_loop_deps_nipy(tmpdir):
     assert recipe_file.isfile()
     assert (nipy_folder / "pypi.json").isfile()
     assert (nipy_folder / "nipy-0.4.2.tar.gz").isfile()
+
+    recipe = Recipe(load_file=recipe_file)
+    assert recipe["extra"]["recipe-maintainers"][0] == "GIT_USER"
