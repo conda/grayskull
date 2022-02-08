c4d00b6ef2019cbdbcd1caf47f6ca0a99e8fbe1f
Marcelo Trevisani <marceloduartetrevisani@gmail.com>
2022-02-08 06:31:44 +0000

    Fix version placeholder for source url when fetching the package from github

---
+++ tests/test_github_recipes.py
@@ -0,0 +1,24 @@
+import pytest
+
+from grayskull.__main__ import create_python_recipe
+from grayskull.strategy.pypi import adjust_source_url_to_include_placeholders
+
+
+@pytest.mark.github
+def test_289_github_url_version_placeholder():
+    recipe, _ = create_python_recipe(
+        "https://github.com/spdx/spdx-license-matcher", version="2.1"
+    )
+    assert (
+        recipe["source"]["url"]
+        == "https://github.com/spdx/spdx-license-matcher/archive/v{{ version }}.tar.gz"
+    )
+
+
+def test_adjust_source_url_to_include_placeholders():
+    assert (
+        adjust_source_url_to_include_placeholders(
+            "https://github.com/spdx/spdx-license-matcher/archive/v2.1.tar.gz", "2.1"
+        )
+        == "https://github.com/spdx/spdx-license-matcher/archive/v{{ version }}.tar.gz"
+    )
