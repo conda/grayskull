223b15af1153624647f1cbe644a8404c283908af
Jannis Leidel <jannis@leidel.info>
2023-02-08 10:25:22 +0100

    Updated URLs after graduation.

--- tests/base/test_github.py
+++ tests/base/test_github.py
@@ -5,4 +5,4 @@ from grayskull.base.github import fetch_all_tags_gh
 
 @pytest.mark.xfail()
 def test_fetch_all_tags_gh():
-    assert len(fetch_all_tags_gh("https://github.com/conda-incubator/grayskull")) > 1
+    assert len(fetch_all_tags_gh("https://github.com/conda/grayskull")) > 1
