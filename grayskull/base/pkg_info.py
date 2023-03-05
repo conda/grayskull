3222796cc5ec8b8a56f6ce5d50288f2de9b0b835
Ben Mares <services-git-throwaway1@tensorial.com>
2023-03-05 23:37:35 +0100

    Delete dead check_pkgs_availability

--- grayskull/base/pkg_info.py
+++ grayskull/base/pkg_info.py
@@ -1,6 +1,4 @@
-import re
 from functools import lru_cache
-from typing import List, Optional, Tuple
 
 import requests
 
@@ -27,35 +25,3 @@ def normalize_pkg_name(pkg_name: str) -> str:
     elif is_pkg_available(pkg_name.replace("_", "-")):
         return pkg_name.replace("_", "-")
     return pkg_name
-
-
-def check_pkgs_availability(
-    list_pkgs: List[str], channel: Optional[str] = None
-) -> List[Tuple[str, bool]]:
-    """Check if the list is
-
-    :param list_pkgs: List with packages name
-    :return:
-    """
-    list_pkgs.sort()
-    re_search = re.compile(r"^\s*[a-z0-9\.\-\_]+", re.IGNORECASE)
-
-    result_list = []
-    all_pkg = set()
-    for pkg in list_pkgs:
-        if not pkg:
-            continue
-        search_result = re_search.search(pkg)
-        if not search_result:
-            continue
-
-        pkg_name = search_result.group()
-        if pkg_name in all_pkg:
-            continue
-
-        all_pkg.add(pkg_name)
-        if channel:
-            result_list.append((pkg, is_pkg_available(pkg_name, channel)))
-        else:
-            result_list.append((pkg, is_pkg_available(pkg_name)))
-    return result_list
