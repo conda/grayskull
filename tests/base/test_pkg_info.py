8a474239634bcc48c7c9476554b6b9fc84741c1e
Ben Mares <services-git-throwaway1@tensorial.com>
2023-03-08 11:41:28 +0100

    Delete dead check_pkgs_availability (#450)

--- tests/base/test_pkg_info.py
+++ tests/base/test_pkg_info.py
@@ -1,4 +1,4 @@
-from grayskull.base.pkg_info import check_pkgs_availability, is_pkg_available
+from grayskull.base.pkg_info import is_pkg_available
 
 
 def test_pkg_available():
@@ -7,23 +7,3 @@ def test_pkg_available():
 
 def test_pkg_not_available():
     assert not is_pkg_available("NOT_PACKAGE_654987321")
-
-
-def test_check_pkgs_availability(capsys):
-    all_pkgs = check_pkgs_availability(
-        [
-            "pytest >=4.0.0,<5.0.0  # [win]",
-            "requests",
-            "pandas[test]",
-            "NOT_PACKAGE_13248743",
-        ]
-    )
-
-    assert sorted(all_pkgs) == sorted(
-        [
-            ("pytest >=4.0.0,<5.0.0  # [win]", True),
-            ("requests", True),
-            ("pandas[test]", True),
-            ("NOT_PACKAGE_13248743", False),
-        ]
-    )
