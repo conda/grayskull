6a8005fca9f379b91ad932c1fca601067732347c
David L. Qiu <david@qiu.dev>
2023-03-26 11:23:09 -0700

    parametrize tests

--- tests/test_poetry.py
+++ tests/test_poetry.py
@@ -17,67 +17,82 @@ from grayskull.strategy.py_toml import (
 )
 
 
-def test_parse_version():
-    assert parse_version("0") == {"major": 0, "minor": None, "patch": None}
-    assert parse_version("1") == {"major": 1, "minor": None, "patch": None}
-    assert parse_version("1.2") == {"major": 1, "minor": 2, "patch": None}
-    assert parse_version("1.2.3") == {"major": 1, "minor": 2, "patch": 3}
+@pytest.mark.parametrize(
+    "version, major, minor, patch",
+    [
+        ("0", 0, None, None),
+        ("1", 1, None, None),
+        ("1.2", 1, 2, None),
+        ("1.2.3", 1, 2, 3),
+    ],
+)
+def test_parse_version_success(version, major, minor, patch):
+    assert parse_version(version) == {"major": major, "minor": minor, "patch": patch}
 
+
+@pytest.mark.parametrize("invalid_version", ["asdf", "", "."])
+def test_parse_version_failure(invalid_version):
     with pytest.raises(InvalidVersion):
-        parse_version("asdf")
-    with pytest.raises(InvalidVersion):
-        parse_version("")
-    with pytest.raises(InvalidVersion):
-        parse_version(".")
+        parse_version(invalid_version)
 
 
-def test_get_caret_ceiling():
+@pytest.mark.parametrize(
+    "version, ceiling_version",
+    [
+        ("0", "1.0.0"),
+        ("0.0", "0.1.0"),
+        ("0.0.3", "0.0.4"),
+        ("0.2.3", "0.3.0"),
+        ("1", "2.0.0"),
+        ("1.2", "2.0.0"),
+        ("1.2.3", "2.0.0"),
+    ],
+)
+def test_get_caret_ceiling(version, ceiling_version):
     # examples from Poetry docs
-    assert get_caret_ceiling("0") == "1.0.0"
-    assert get_caret_ceiling("0.0") == "0.1.0"
-    assert get_caret_ceiling("0.0.3") == "0.0.4"
-    assert get_caret_ceiling("0.2.3") == "0.3.0"
-    assert get_caret_ceiling("1") == "2.0.0"
-    assert get_caret_ceiling("1.2") == "2.0.0"
-    assert get_caret_ceiling("1.2.3") == "2.0.0"
+    assert get_caret_ceiling(version) == ceiling_version
 
 
-def test_get_tilde_ceiling():
+@pytest.mark.parametrize(
+    "version, ceiling_version",
+    [("1", "2.0.0"), ("1.2", "1.3.0"), ("1.2.3", "1.3.0")],
+)
+def test_get_tilde_ceiling(version, ceiling_version):
     # examples from Poetry docs
-    assert get_tilde_ceiling("1") == "2.0.0"
-    assert get_tilde_ceiling("1.2") == "1.3.0"
-    assert get_tilde_ceiling("1.2.3") == "1.3.0"
+    assert get_tilde_ceiling(version) == ceiling_version
 
 
-def test_encode_poetry_version():
-    # should be unchanged
-    assert encode_poetry_version("1.*") == "1.*"
-    assert encode_poetry_version(">=1,<2") == ">=1,<2"
-    assert encode_poetry_version("==1.2.3") == "==1.2.3"
-    assert encode_poetry_version("!=1.2.3") == "!=1.2.3"
-
-    # strip spaces
-    assert encode_poetry_version(">= 1, < 2") == ">=1,<2"
-
-    # handle exact version specifiers correctly
-    assert encode_poetry_version("1.2.3") == "1.2.3"
-    assert encode_poetry_version("==1.2.3") == "==1.2.3"
-
-    # handle caret operator correctly
-    # examples from Poetry docs
-    assert encode_poetry_version("^0") == ">=0.0.0,<1.0.0"
-    assert encode_poetry_version("^0.0") == ">=0.0.0,<0.1.0"
-    assert encode_poetry_version("^0.0.3") == ">=0.0.3,<0.0.4"
-    assert encode_poetry_version("^0.2.3") == ">=0.2.3,<0.3.0"
-    assert encode_poetry_version("^1") == ">=1.0.0,<2.0.0"
-    assert encode_poetry_version("^1.2") == ">=1.2.0,<2.0.0"
-    assert encode_poetry_version("^1.2.3") == ">=1.2.3,<2.0.0"
-
-    # handle tilde operator correctly
-    # examples from Poetry docs
-    assert encode_poetry_version("~1") == ">=1.0.0,<2.0.0"
-    assert encode_poetry_version("~1.2") == ">=1.2.0,<1.3.0"
-    assert encode_poetry_version("~1.2.3") == ">=1.2.3,<1.3.0"
+@pytest.mark.parametrize(
+    "version, encoded_version",
+    [
+        # should be unchanged
+        ("1.*", "1.*"),
+        (">=1,<2", ">=1,<2"),
+        ("==1.2.3", "==1.2.3"),
+        ("!=1.2.3", "!=1.2.3"),
+        # strip spaces
+        (">= 1, < 2", ">=1,<2"),
+        # handle exact version specifiers correctly
+        ("1.2.3", "1.2.3"),
+        ("==1.2.3", "==1.2.3"),
+        # handle caret operator correctly
+        # examples from Poetry docs
+        ("^0", ">=0.0.0,<1.0.0"),
+        ("^0.0", ">=0.0.0,<0.1.0"),
+        ("^0.0.3", ">=0.0.3,<0.0.4"),
+        ("^0.2.3", ">=0.2.3,<0.3.0"),
+        ("^1", ">=1.0.0,<2.0.0"),
+        ("^1.2", ">=1.2.0,<2.0.0"),
+        ("^1.2.3", ">=1.2.3,<2.0.0"),
+        # handle tilde operator correctly
+        # examples from Poetry docs
+        ("~1", ">=1.0.0,<2.0.0"),
+        ("~1.2", ">=1.2.0,<1.3.0"),
+        ("~1.2.3", ">=1.2.3,<1.3.0"),
+    ],
+)
+def test_encode_poetry_version(version, encoded_version):
+    assert encode_poetry_version(version) == encoded_version
 
 
 def test_add_poetry_metadata():
