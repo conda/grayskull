6b2b2149b17bf6f4574e8ce0a145d36f93024c94
Jack Olivieri <jolivieri@anaconda.com>
2023-08-09 09:08:38 +0200

    align with current standard build script command

--- grayskull/strategy/pypi.py
+++ grayskull/strategy/pypi.py
@@ -59,7 +59,7 @@ class PypiStrategy(AbstractStrategy):
     def fetch_data(recipe, config, sections=None):
         update_recipe(recipe, config, sections or ALL_SECTIONS)
         if not (recipe["build"] and recipe["build"]["script"]):
-            recipe["build"]["script"] = "<{ PYTHON }} -m pip install . -vv"
+            recipe["build"]["script"] = "<{ PYTHON }} -m pip install . -vv --no-deps --no-build-isolation"
 
 
 def merge_pypi_sdist_metadata(
