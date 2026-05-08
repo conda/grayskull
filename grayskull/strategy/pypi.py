fb0b819598f215375f6b7da5dfaaaf1bf07f3a21
Benjamin Bertrand <beenje@gmail.com>
2026-05-08 10:16:42 +0200

    Clean code in update_recipe

--- grayskull/strategy/pypi.py
+++ grayskull/strategy/pypi.py
@@ -587,13 +587,10 @@ def update_recipe(recipe: Recipe, config: Configuration, all_sections: Iterable[
         metadata[section] = remove_all_inner_nones(metadata.get(section, {}))
         if metadata.get(section):
             if section == "package":
+                # Update both name and version keys in that section
+                # name is usually correct in the initial recipe but
+                # might not in some cases (local sdist)
                 package_metadata = dict(metadata[section])
-                # In case of from_local_sdist, the initial name set in the recipe
-                # came from the sdist filename and shall be updated.
-                # No need otherwise.
-                if not config.from_local_sdist:
-                    package_metadata.pop("name")
-
                 set_global_jinja_var(recipe, "version", package_metadata["version"])
                 config.version = package_metadata["version"]
                 package_metadata["version"] = "<{ version }}"
